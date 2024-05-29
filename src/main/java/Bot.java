bd6802de111efea3d07f750179da9088b59b9335
ogar_m <ogar.m.v@mail.ru>
2024-05-29 11:23:42 +0300

    tg bot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
+    private boolean screaming = false;
     @Override
     public String getBotUsername() {
         return "Tutorial tg bot";
@@ -25,21 +26,44 @@ public class Bot extends TelegramLongPollingBot {
 
         //sendText(id, msg.getText());
         System.out.println(user.getFirstName() + " wrote " + msg.getText() + " :)");
-        copyMessage(userId, msg.getMessageId());
+
+        if (msg.isCommand()) {
+            if (msg.getText().equals("/scream")) {
+                screaming = true; //If the command was "scream", we switch gears
+            } else if (msg.getText().equals("/whisper")) {
+                screaming = false; //Otherwise we return to normal
+            }
+            return; //We don't want to echo commands, so we exit
+        }
+
+        if (screaming) {
+            scream(userId, msg); //Call a custom method
+        } else {
+            copyMessage(userId, msg.getMessageId()); //Else proceed normally
+        }
+
     }
 
-//    public void sendText(Long who, String what) {
-//        SendMessage sm = SendMessage.builder()
-//                .chatId(who.toString()) //who are we sending a message to
-//                .text(what) //Content message
-//                .build();
-//
-//        try {
-//            execute(sm); //Actually sending the message
-//        } catch (TelegramApiException tae) {
-//            throw new RuntimeException(); //Any error will be printed here
-//        }
-//    }
+    private void scream(Long id, Message msg) {
+        if (msg.hasText()) {
+            sendText(id, msg.getText().toUpperCase());
+        } else {
+            copyMessage(id, msg.getMessageId()); //We can't really scream a sticker
+        }
+    }
+
+    public void sendText(Long who, String what) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //who are we sending a message to
+                .text(what) //Content message
+                .build();
+
+        try {
+            execute(sm); //Actually sending the message
+        } catch (TelegramApiException tae) {
+            throw new RuntimeException(); //Any error will be printed here
+        }
+    }
 
     public void copyMessage(Long who, Integer idMsg) {
         CopyMessage cm = CopyMessage.builder()
